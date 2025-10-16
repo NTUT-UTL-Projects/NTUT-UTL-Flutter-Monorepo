d9c7985e551942b58f02b64b38d1bee31887dc15
XIAN-SHENG-576692 <chenxiansheng576692@gmail.com>
2025-10-16 23:16:19 +0800

    feat(stream_utils): dart format.

--- packages/stream_utils/lib/src/merge_stream.dart
+++ packages/stream_utils/lib/src/merge_stream.dart
@@ -24,7 +24,11 @@ Stream<T> mergeStreams<T>(List<Stream<T>> streams) {
   }
 
   void subscribeToStream(Stream<T> stream) {
-    final s = stream.listen(handleData, onError: handleError, onDone: handleDone);
+    final s = stream.listen(
+      handleData,
+      onError: handleError,
+      onDone: handleDone,
+    );
     subscriptions.add(s);
   }
 
